08ce946aa83bb7cfb96e606ec8712d13c8004d6d
Eldain <white.wulf4@gmail.com>
2025-05-23 14:58:16 +0200

    Update interaction_curve_v2.py

--- interaction_curve_v2.py
+++ interaction_curve_v2.py
@@ -37,9 +37,9 @@ with st.expander("ℹ️ Click here for Instructions and Explanations"):
     st.latex(r"\frac{M_{ux}}{\phi M_{nx}} + \frac{M_{uy}}{\phi M_{ny}} \leq 1.0")
     
     st.markdown("""
-    - $M_{ux}$, $M_{uy}$: factored applied moments about the x- and y-axes.  
-    - $M_{nx}$, $M_{ny}$: nominal moment capacities about the x- and y-axes.  
-    - $\\phi$: strength reduction factor.
+                - $M_{ux}$, $M_{uy}$: factored applied moments about the x- and y-axes.  
+                - $M_{nx}$, $M_{ny}$: nominal moment capacities about the x- and y-axes.  
+                - $\\phi$: strength reduction factor.
     """)
 
     
